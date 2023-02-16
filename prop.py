51cf77d18cf5d34da8a486b553b776feae882968
Grocery4 <113478478+Grocery4@users.noreply.github.com>
2023-02-16 22:14:17 +0100

    Created simple window

--- prop.py
+++ prop.py
@@ -1,8 +1,24 @@
 
-
+import PySimpleGUI as sg
 
 def main():
 
-    print("Hello python, it's been a while")
+    margin_x = 500
+    margin_y = 300
+
+    layout = [
+        [sg.Text("Welcome to Ru's converter!")],
+        [sg.Button("Convert")]  
+    ]
+
+    window = sg.Window("Ru's Converter", layout, margins=(margin_x, margin_y))
+
+    while True:
+        event, values = window.read()
+        if event == sg.WIN_CLOSED:
+            break
+
+    window.close()
+
 
 main()
